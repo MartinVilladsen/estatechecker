16c5d5460e9d2dbc224f8a24511d7ff56c8da503
Martin Knak Villadsen <martin.k.villadsen@gmail.com>
2025-11-17 19:04:33 +0100

    feat: add wrapper for boliga api results ignoring the meta data

---
+++ src/main/java/com/example/estatechecker/model/wrapper/BoligaApiResults.java
@@ -0,0 +1,12 @@
+package com.example.estatechecker.model.wrapper;
+
+import com.example.estatechecker.model.dto.BoligaEstateDto;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+/** Wrapper for BoligaEstate, i am ignoring meta data from results */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record BoligaApiResults(
+        List<BoligaEstateDto> results
+) {}
